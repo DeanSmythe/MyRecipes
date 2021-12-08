958c20f24b620e0cddc09f4aa2f6037c1a77f469
Jamie Bowers <jamiebowers3@gmail.com>
2021-12-08 15:29:58 +0000

    in progress

---
+++ app/src/main/java/com/example/myrecipes/activity_recipe_home_page.kt
@@ -0,0 +1,11 @@
+package com.example.myrecipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_recipe_home_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_home_page)
+    }
+}
