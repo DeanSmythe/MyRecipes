3ce6a256ae29ad09044d13058821f737042391ed
Jenaro Garcia <jjegarcia@hotmail.com>
2021-12-09 16:15:41 +0000

    WIP added storage driver

---
+++ app/src/main/java/com/example/myrecipes/data/model/StorageHandler.kt
@@ -0,0 +1,10 @@
+package com.example.myrecipes.data.model
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+class StorageHandler {
+    var storage = Firebase.storage
+
+
+}
