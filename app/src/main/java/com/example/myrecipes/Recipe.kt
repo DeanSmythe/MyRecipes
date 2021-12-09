0af82ebff9470c895179f5a0e94ab03cb37a2031
Dean Smythe <dean@teamtwisted.co.uk>
2021-12-09 15:12:27 +0000

    Recipe class added

--- app/src/main/java/com/example/myrecipes/Recipe.kt
+++ app/src/main/java/com/example/myrecipes/Recipe.kt
@@ -7,18 +7,25 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.firestore.IgnoreExtraProperties
 
 @IgnoreExtraProperties
-data class Ingredient(
+data class Recipe(
     val name: String? = null,
     val description: String? = null,
-    val uom: String? = null,
-    val picture: String? = null
+    val timetomake: Int? = null,
+    val picture: String? = null,
+    val rating: Int? = null
 ) {
 
     private val db = Firebase.firestore
 
-    fun writeNewIngredient(name: String, description: String, uom: String, picture: String) {
-        val ingredient = Ingredient(name, description, uom, picture)
-        db.collection("ingredients").add(ingredient)
+    fun setNewRecipe(
+        name: String,
+        description: String,
+        timetomake: Int,
+        picture: String,
+        rating: Int
+    ) {
+        val recipe = Recipe(name, description, timetomake, picture, rating)
+        db.collection("recipes").add(recipe)
             .addOnSuccessListener { documentReference ->
                 Log.d(ContentValues.TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
             }
