28e50ccf01d6b203c1a76b24051cd805cf92c434
Jamie Bowers <jamiebowers3@gmail.com>
2021-12-10 16:24:18 +0000

    Cupboard set up with pictures

--- app/src/main/java/com/example/myrecipes/MyCupboard.kt
+++ app/src/main/java/com/example/myrecipes/MyCupboard.kt
@@ -10,15 +10,52 @@ import androidx.core.view.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+
+
 class MyCupboard : AppCompatActivity() {
     private lateinit var ingredientAdapter: IngredientItemAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_cupboard)
         ingredientAdapter = IngredientItemAdapter(mutableListOf())
 
+        setUpRecyclerAndCards()
+
+        val spinner = findViewById<Spinner>(R.id.spinnerIngredients)
+        val spinnerUom = findViewById<Spinner>(R.id.spinneruom)
+
+        setSpinnerAdapter(spinner,spinnerUom)
+
+    }
+
+    private fun setSpinnerAdapter(spinner: Spinner, spinnerUom: Spinner){
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.ingredients,
+            android.R.layout.simple_spinner_item
+        ).also { arrayAdapter -> arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = arrayAdapter}
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.uom,
+            android.R.layout.simple_spinner_item
+        ).also { arrayAdapter -> arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerUom.adapter = arrayAdapter}
+    }
+
+    private fun findImage(ingredient: Spinner) : String?{
+        val map = mapOf("https://live.staticflickr.com/3241/3083459599_55d24a48f8.jpg" to "Eggs",
+            "https://thumbs.dreamstime.com/b/flour-glass-bowl-wheat-isolated-white-36638830.jpg" to "Flour",
+            "https://static7.depositphotos.com/1001069/716/i/600/depositphotos_7169901-stock-photo-milk-bottle.jpg" to "Milk"
+        )
+        return map.entries.find { it.value == ingredient.selectedItem.toString()}?.key
+    }
+
+    private fun setUpRecyclerAndCards(){
         val listOfIngredients = findViewById<RecyclerView>(R.id.rvListOfIngredients)
         listOfIngredients.adapter = ingredientAdapter
         listOfIngredients.layoutManager = LinearLayoutManager(this)
@@ -29,31 +66,11 @@ class MyCupboard : AppCompatActivity() {
             val ingredient = findViewById<Spinner>(R.id.spinnerIngredients)
             val ingredientUom = findViewById<Spinner>(R.id.spinneruom)
             if (quantity.isNotEmpty()) {
-                val ingredient = Ingredient(picture = "https://live.staticflickr.com/3241/3083459599_55d24a48f8.jpg",
+                val ingredient = Ingredient(picture = findImage(ingredient= ingredient) ,
                     description = quantity,uom = ingredientUom.selectedItem.toString(), name = ingredient.selectedItem.toString())
                 ingredientAdapter.addIngredient(ingredient)
                 findViewById<EditText>(R.id.etIngredientQuantity).setText("")
             }
         }
-        val spinner = findViewById<Spinner>(R.id.spinnerIngredients)
-
-//        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, resources.getStringArray(R.array.ingredients))
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.ingredients,
-            android.R.layout.simple_spinner_item
-        ).also { arrayAdapter -> arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = arrayAdapter}
-
-        val spinnerUom = findViewById<Spinner>(R.id.spinneruom)
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.uom,
-            android.R.layout.simple_spinner_item
-        ).also { arrayAdapter -> arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinnerUom.adapter = arrayAdapter}
-
-
-
     }
 }
