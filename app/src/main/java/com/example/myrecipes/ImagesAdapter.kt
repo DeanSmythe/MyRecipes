9897f051314ac00cef8b1c403b1967478eca092f
Jenaro Garcia <jjegarcia@hotmail.com>
2021-12-11 19:49:38 +0000

    refactor images adapter

--- app/src/main/java/com/example/myrecipes/ImagesAdapter.kt
+++ app/src/main/java/com/example/myrecipes/ImagesAdapter.kt
@@ -9,7 +9,10 @@ import com.example.myrecipes.data.Image
 
 class ImagesAdapter constructor(val images: MutableList<Image>) : RecyclerView.Adapter<ImagesAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageNameTextView = itemView.findViewById<TextView>(R.id.imageNameTextView)
+        val imageUrlTextView = itemView.findViewById<TextView>(R.id.imageUrlTextView)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val item = LayoutInflater.from(parent.context).inflate(R.layout.image_item, parent, false)
@@ -17,13 +20,11 @@ class ImagesAdapter constructor(val images: MutableList<Image>) : RecyclerView.A
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView.apply {
-            val name=findViewById<TextView>(R.id.imageNameTextView)
-            name.text = images[position].imageName
-            val url= findViewById<TextView>(R.id.imageUrlTextView)
-            url.text = images[position].imageUrl
+        holder.apply {
+            imageNameTextView.text = images[position].imageName
+            imageUrlTextView.text = images[position].imageUrl
         }
-            }
+    }
 
     override fun getItemCount() = images.size
 
